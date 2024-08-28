12b9c89404fa1e60528bdc92208dd1fdec92ad86
Temitayo Badewole <89653882+tayo2910@users.noreply.github.com>
2024-08-28 12:26:53 +0100

    first commit

---
+++ class.js
@@ -0,0 +1,16 @@
+// for (iterator; condition; iteration);
+
+// for(const value of values);
+
+// function functionName(parameters) {
+//     // logic goes here
+// }
+
+
+const rows = ["Tumininu", 12, "fayo"]
+rows = "Ayo"
+console.log(rows)
+
+// const rows = ["Tumininu", 12, "fayo"]
+// rows = ["Ayo", 23]
+// console.log(rows)
